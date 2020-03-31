914ae6f87d189c2496c8c99cf7075ee782ce25fe
Annika Mauro <amauro@stanford.edu>
2020-03-31 14:08:12 -0700

    Undoing fixes

--- init.el
+++ init.el
@@ -5,18 +5,26 @@
       initial-major-mode 'fundamental-mode
       initial-scratch-message nil)
 
+(advice-add #'display-startup-echo-area-message :override #'ignore)
+(setq inhibit-startup-message t
+      inhibit-startup-echo-area-message user-login-name
+      inhibit-default-init t
+      initial-major-mode 'fundamental-mode
+      initial-scratch-message nil)
+
 (let ((file-name-handler-alist nil)
       (orgfile (expand-file-name "gmacs.org" user-emacs-directory))
       (elfile (expand-file-name "gmacs.el" user-emacs-directory))
       (elcfile (expand-file-name "gmacs.elc" user-emacs-directory)))
   (if (file-exists-p elcfile)
       (load-file elcfile)
-    (when (or (not (file-exists-p elfile))
-              (file-newer-than-file-p orgfile elfile))
-      (progn
-        (require 'org)
-        (org-babel-tangle-file orgfile)
-        (when (file-exists-p personal-file) (load-file personal-file))))
-    (load-file elfile)))
+    (if (or (not (file-exists-p elfile))
+            (file-newer-than-file-p orgfile elfile))
+        (progn
+          (require 'org)
+          (org-babel-load-file orgfile))
+      (load-file elfile))))
 
-(when (file-exists-p personal-file) (load-file personal-file))
+(let ((personalfile (expand-file-name "personal.el" user-emacs-directory)))
+  (when (file-exists-p personalfile)
+    (load-file personalfile)))
